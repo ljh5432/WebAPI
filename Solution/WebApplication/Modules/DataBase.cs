a54e4e3b25130a1a94287492761609ebf64191b7
지현 이 <ljh5432@gmail.com>
2019-02-08 13:49:38 +0900

    sad

---
+++ Solution/WebApplication/Modules/DataBase.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Modules
+{
+    public class DataBase
+    {
+        private MySqlConnection conn = null;
+
+        public DataBase()
+        {
+            GetConnection();
+        }
+
+        public void ConnectionClose()
+        {
+            conn.Close();
+        }
+
+        private bool GetConnection()
+        {
+            conn = new MySqlConnection();
+            conn.ConnectionString
+                = string.Format("server={0};user={1};password={2};database={3};port={4};", "192.168.3.150", "root", "1234", "test", "3306");
+            try
+            {
+                conn.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public MySqlDataReader GetReader(string sql)
+        {
+            try
+            {
+                MySqlCommand comm = new MySqlCommand();
+                comm.CommandText = sql;
+                comm.Connection = conn;
+                return comm.ExecuteReader();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+    }
+}
